d07da0664a5d7712174dd8793cfd26d29fef8208
unknown <A.com>
2018-05-01 13:46:57 -0400

    added vr callibration

---
+++ Assets/VRCamCalibration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCamCalibration : MonoBehaviour {
+
+	public Camera VRCam;
+	public Transform Player;
+	public Vector3 Offset;
+	// Use this for initialization
+
+
+	void CalibrateCam() {
+		Player.parent = null;
+		transform.localPosition = VRCam.transform.localPosition + Offset;
+		Player.parent = transform;
+//		transform.localPosition = VRCam.transform.localPosition;
+	}
+	void Start () {
+		CalibrateCam ();
+	}
+
+}
